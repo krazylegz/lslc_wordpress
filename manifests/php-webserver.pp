a4e97a5337f8d08fb54ec2a84aa482c5fbb94cd2
Vikram Dighe <vikram@swiftsignal.com>
2014-12-28 16:07:22 -0500

    Reformatting for readability.

--- manifests/php-webserver.pp
+++ manifests/php-webserver.pp
@@ -5,12 +5,46 @@ class php-webserver {
   package { php5-fpm: ensure => latest }
   package { php5-mysql: ensure => latest }
 
-  file { '/etc/nginx/sites-available/default': ensure => 'file', mode => '0644', owner => 'root', group => 'root', source => '/vagrant/files/etc/nginx/sites-available/default', require => Package['nginx'], notify => Service['nginx'] }
-  file { '/etc/php5/fpm/php.ini': ensure => 'file', mode => '0644', owner => 'root', group => 'root', source => '/vagrant/files/etc/php5/fpm/php.ini', require => Package['php5-fpm'], notify => Service['php5-fpm'] }
+  file { '/etc/nginx/sites-available/default':
+    ensure => 'file',
+    mode => '0644',
+    owner => 'root',
+    group => 'root',
+    source => '/vagrant/files/etc/nginx/sites-available/default',
+    require => Package['nginx'],
+    notify => Service['nginx']
+  }
+  file { '/etc/php5/fpm/php.ini':
+    ensure => 'file',
+    mode => '0644',
+    owner => 'root',
+    group => 'root',
+    source => '/vagrant/files/etc/php5/fpm/php.ini',
+    require => Package['php5-fpm'],
+    notify => Service['php5-fpm']
+  }
 
-  group { 'www-data': ensure => present, gid => 33 }
-  user { 'www-data': ensure => present, gid => 'www-data', groups => [ 'www-data' ], membership => minimum, shell => '/usr/sbin/nologin', require => Group['www-data'] }
+  group { 'www-data':
+    ensure => present,
+    gid => 33
+  }
+  user { 'www-data':
+    ensure => present,
+    gid => 'www-data',
+    groups => [ 'www-data' ],
+    membership => minimum,
+    shell => '/usr/sbin/nologin',
+    require => Group['www-data']
+  }
 
-  service { 'nginx': ensure => 'running', enable => 'true', require => Package['nginx'] }
-  service { 'php5-fpm': ensure => 'running', enable => 'true', require => Package['php5-fpm'] }
+  service { 'nginx':
+    ensure => 'running',
+    enable => 'true',
+    require => Package['nginx']
+  }
+  service { 'php5-fpm':
+    ensure => 'running',
+    enable => 'true',
+    require => Package['php5-fpm']
+  }
 }
