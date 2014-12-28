6fb0e437480da5691035d1372d4f6b8979470221
Vikram Dighe <vikram@swiftsignal.com>
2014-12-28 16:07:22 -0500

    Reformatting for readability.

--- manifests/wordpress.pp
+++ manifests/wordpress.pp
@@ -3,8 +3,21 @@ import 'php-webserver'
 node default {
   include php-webserver
   
-  file { '/var/www/': ensure => 'directory', owner => 'www-data', group => 'www-data', mode => 755, source => '/vagrant/files/wordpress', recurse => true }
-  file { '/tmp/lslc_wordpress.sql': ensure => 'file', mode => '0644', owner => 'root', group => 'root', source => '/vagrant/files/lslc_wordpress.sql' }
+  file { '/var/www/':
+    ensure => 'directory',
+    owner => 'www-data',
+    group => 'www-data',
+    mode => 755,
+    source => '/vagrant/files/wordpress',
+    recurse => true
+  }
+  file { '/tmp/lslc_wordpress.sql':
+    ensure => 'file',
+    mode => '0644',
+    owner => 'root',
+    group => 'root',
+    source => '/vagrant/files/lslc_wordpress.sql'
+  }
 
   mysql::db { 'lslc_wordpress':
     user => 'lslc',
